74daa66192a39eed011bcdc1a23046380c1c98a0
Paul Talbot <paul@fullfrontalingenuity.com>
2015-09-24 19:47:45 -0400

    sample app with DB creds

--- include/membersite_config.php
+++ include/membersite_config.php
@@ -4,23 +4,23 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('example.com');
 
 //Provide the email address where you want to get notifications
-$fgmembersite->SetAdminEmail('user11@user11.com');
+$fgmembersite->SetAdminEmail('admin@example.com');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
 $fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
-                      /*database name*/'testdb',
+                      /*username*/'u_app1_devtest',
+                      /*password*/'secret_password',
+                      /*database name*/'devtest',
                       /*table name*/'fgusers3');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
-$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+$fgmembersite->SetRandomKey('^XXqSR~!#(EUI#HE(&cVS6DrTzrPvrXX^');
 
 ?>
