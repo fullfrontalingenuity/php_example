73a189d5a8f21189b175a9e53710d78ec4f1fec2
Paul Talbot <paul@fullfrontalingenuity.com>
2015-09-24 18:56:38 -0400

    updated index

--- index.php
+++ index.php
@@ -7,13 +7,37 @@
   }
 ?>
 <html>
-   <body>
-      <form action="<?php $_PHP_SELF ?>" method="GET">
-	 Name: <input type="text" name="name" />
-	 Age: <input type="text" name="age" />
-	 <input type="submit" />
-      </form>
-   </body>
+  <head>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <form action="<?php $_PHP_SELF ?>" method="GET">
+ Name: <input type="text" name="name" />
+ Age: <input type="text" name="age" />
+      <input type="submit" />
+    </form>
+    <form class="login">
+      <fieldset>
+        <legend class="legend">Login</legend>
+          <div class="input">
+            <input type="email" placeholder="Email" required />
+            <span><i class="fa fa-envelope-o"></i></span>
+          </div>
+
+          <div class="input">
+            <input type="password" placeholder="Password" required />
+            <span><i class="fa fa-lock"></i></span>
+          </div>
+
+          <button type="submit" class="submit"><i class="fa fa-long-arrow-right"></i></button>
+
+      </fieldset>
+      <div class="feedback">
+login successful <br />
+redirecting...
+      </div>
+    </form>
+  </body>
 </html>
 <?php
 phpinfo()
