74505bc99464dda2889c3079e9628fe5cfff9154
Paul Talbot <paul@fullfrontalingenuity.com>
2015-09-25 16:47:53 -0400

    adding navbar

--- include/navbar.php
+++ include/navbar.php
@@ -1,4 +1,7 @@
 <?php
+
+  require_once("./include/membersite_config.php");
+
   function modifyNavbar($items) {
     $ref = isset($_GET['p']) && isset($items[$_GET['p']]) ? $_GET['p'] : null;
     if($ref) {
@@ -7,17 +10,26 @@
     return $items;
   }
 
-  $menu = array(
-    'callback' => 'modifyNavbar',
-    'items' => array(
-      'home'  => array('text'=>'Home',  'url'=>'/?p=home', 'class'=>null),
-      'register'  => array('text'=>'Register',  'url'=>'/register.php?p=register', 'class'=>null),
-      'confirm' => array('text'=>'Confirm', 'url'=>'/confirm.php?p=confirm', 'class'=>null),
-      'login' => array('text'=>'Login', 'url'=>'/login.php?p=login', 'class'=>null),
-      'test' => array('text'=>'Test', 'url'=>'/access-controlled.php?p=test', 'class'=>null),
-      'logout' => array('text'=>'Logout', 'url'=>'/logout.php?p=logout', 'class'=>null),
-    ),
-  );
+  if($fgmembersite->CheckLogin()) {
+    $menu = array(
+      'callback' => 'modifyNavbar',
+      'items' => array(
+        'home'  => array('text'=>'Home',  'url'=>'/?p=home', 'class'=>null),
+        'test' => array('text'=>'Test', 'url'=>'/access-controlled.php?p=test', 'class'=>null),
+        'logout' => array('text'=>'Logout', 'url'=>'/logout.php?p=logout', 'class'=>null),
+      ),
+    );    
+  } else {
+    $menu = array(
+      'callback' => 'modifyNavbar',
+      'items' => array(
+        'home'  => array('text'=>'Home',  'url'=>'/?p=home', 'class'=>null),
+        'register'  => array('text'=>'Register',  'url'=>'/register.php?p=register', 'class'=>null),
+        'confirm' => array('text'=>'Confirm', 'url'=>'/confirmreg.php?p=confirm', 'class'=>null),
+        'login' => array('text'=>'Login', 'url'=>'/login.php?p=login', 'class'=>null),
+      ),
+    );
+  }
 
   class CNavigation {
     public static function GenerateMenu($menu, $class) {
