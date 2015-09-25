fb57bf62ae35f9ab90529366ebfd2c425c97772b
Paul Talbot <paul@fullfrontalingenuity.com>
2015-09-25 16:25:01 -0400

    adding branch to index

--- include/navbar.php
+++ include/navbar.php
@@ -1,20 +1,35 @@
 <?php
+  function modifyNavbar($items) {
+    $ref = isset($_GET['p']) && isset($items[$_GET['p']]) ? $_GET['p'] : null;
+    if($ref) {
+      $items[$ref]['class'] .= 'selected'; 
+    }
+    return $items;
+  }
+
   $menu = array(
-    'home'  => array('text'=>'Home',  'url'=>'?p=home'),
-    'away'  => array('text'=>'Away',  'url'=>'?p=away'),
-    'about' => array('text'=>'About', 'url'=>'?p=about'),
+    'callback' => 'modifyNavbar',
+    'items' => array(
+      'home'  => array('text'=>'Home',  'url'=>'?p=home', 'class'=>null),
+      'away'  => array('text'=>'Away',  'url'=>'?p=away', 'class'=>null),
+      'about' => array('text'=>'About', 'url'=>'?p=about', 'class'=>null),
+    ),
   );
 
   class CNavigation {
-    public static function GenerateMenu($items, $class) {
+    public static function GenerateMenu($menu, $class) {
+      if(isset($menu['callback'])) {
+        $items = call_user_func($menu['callback'], $menu['items']);
+      }
       $html = "<nav class='$class'>\n";
-      foreach($items as $key => $item) {
-        $selected = (isset($_GET['p'])) && $_GET['p'] == $key ? 'selected' : null; 
-        $html .= "<a href='{$item['url']}' class='{$selected}'>{$item['text']}</a>\n";
+      foreach($items as $item) {
+        $html .= "<a href='{$item['url']}' class='{$item['class']}'>{$item['text']}</a>\n";
       }
       $html .= "</nav>\n";
       return $html;
-    }
+   }    
   };
-  echo CNavigation::GenerateMenu($menu);
+  echo "<nav class='navbar'>";
+  echo CNavigation::GenerateMenu($menu, $class);
+  echo "</nav>";
 ?>
