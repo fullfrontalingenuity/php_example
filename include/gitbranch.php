29d1bb3dee798d1861261b457e1ec0f06f0f18a7
Paul Talbot <paul@fullfrontalingenuity.com>
2015-09-25 12:32:46 -0400

    adding branch to index

--- include/gitbranch.php
+++ include/gitbranch.php
@@ -5,12 +5,12 @@
  *
  ****************************/
 
-function GitBranch($branchname)
+function GitBranch()
 {
   $stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
   $firstLine = $stringfromfile[0]; //get the string from the array
   $explodedstring = explode("/", $firstLine, 3); //seperate out by the "/" in the string
   $branchname = $explodedstring[2]; //get the one that is always the branch name
-  return $branchname;
+  echo $branchname;
 } 
 ?>
