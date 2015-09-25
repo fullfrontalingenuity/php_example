33a18a50f654260faa8268e0d84d5a79dd9f89f4
Paul Talbot <paul@fullfrontalingenuity.com>
2015-09-25 15:04:19 -0400

    adding branch to index

--- include/gitbranch.php
+++ include/gitbranch.php
@@ -11,18 +11,21 @@ function GitBranch()
   $firstLine = $stringfromfile[0]; //get the string from the array
   $explodedstring = explode("/", $firstLine, 3); //seperate out by the "/" in the string
   $branchname = $explodedstring[2]; //get the one that is always the branch name
+  $branchname = str_replace(array("\r", "\n"), '', $branchname);
   switch ($branchname) {
-    case "devtest":
+    case 'devtest':
       $background = "#bcbf77";
       break;
-    case "staging":
+    case 'staging':
       $background = "#685aff";
       break;
-    case "prod":
+    case 'prod':
       $background = "#f1ff5a";
       break;
     default:
+      $branchname = "unexpected branch detected: " .$branchname;
       $background = "#ff625a";
+      break;
   }
   echo "<div style='clear: both; width: 100%; font-size: 14px; font-family: Helvetica; color: #30121d; background: ", $background, "; padding: 20px; text-align: center;'>";
   echo "<span style='color:#fff; font-weight: bold; text-transform: uppercase;'>", $branchname, "</span>";
