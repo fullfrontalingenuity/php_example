7a04d6768846d6226dc68727a9b9de9f36578768
Paul Talbot <paul@fullfrontalingenuity.com>
2015-09-29 14:42:28 -0400

    moved phpinfo to bottom of test

--- test.php
+++ test.php
@@ -18,17 +18,15 @@ if(!$fgmembersite->CheckLogin())
 <?php
   include './include/header.php';
 ?>
-<div id='fg_membersite_content'>
-<h2>Test MySQL</h2>
+  <div id='fg_membersite_content'>
+  <h2>Test MySQL</h2>
 Use root MySQL password to start a test of MySQL using php.
-</div>
+  </div>
 
 <?php
 
 // Validate minimal LAMP functionality
 // 2014-10-27  Ron Compos
-
-phpinfo();
 
 $dbserver   = "localhost";
 $dbuser     = "root";
@@ -48,102 +46,103 @@ echo "<br>";
 
 if (isset($_POST["password"])){ 
 
-$user= $_POST["user"];
-if($user){
-   $dbuser = $user;
-   echo "<b>MySQL user</b>: $dbuser<br>";
-} else {
-   echo "<b>MySQL user</b>: $dbuser<br>";
-}
+  $user= $_POST["user"];
+  if($user){
+    $dbuser = $user;
+    echo "<b>MySQL user</b>: $dbuser<br>";
+  } else {
+    echo "<b>MySQL user</b>: $dbuser<br>";
+  }
 
-$password = $_POST["password"];
-if($password){
-   $dbpw = $password;
-   echo "<b>User-input MySQL password</b><br>";
-} else {
-   echo "<b>Default MySQL password</b><br>";
-}
+  $password = $_POST["password"];
+  if($password){
+    $dbpw = $password;
+    echo "<b>User-input MySQL password</b><br>";
+  } else {
+    echo "<b>Default MySQL password</b><br>";
+  }
 
-//echo "<b>MySQL pw:</b> $dbpw<br>";
+  //echo "<b>MySQL pw:</b> $dbpw<br>";
 
-echo "<b>Connect to database server:</b> " . $dbserver . "<br>";
-$link = mysql_connect($dbserver, $dbuser, $dbpw) or die('<b>Could not connect:</b> ' . mysql_error() . '<br>');
-echo "<b>Connected successfully</b><br>";
+  echo "<b>Connect to database server:</b> " . $dbserver . "<br>";
+  $link = mysql_connect($dbserver, $dbuser, $dbpw) or die('<b>Could not connect:</b> ' . mysql_error() . '<br>');
+  echo "<b>Connected successfully</b><br>";
 
-$r0 = mysql_select_db( $db );
-if (!$r0) {
+  $r0 = mysql_select_db( $db );
+  if (!$r0) {
     echo "<b>Cannot select database</b><br>";
     echo "<b>Database test must exist!</b><br>";
     trigger_error(mysql_error(), E_USER_ERROR); 
-} else {
+  } else {
     echo "<b>Database selected:</b> " . $db . "<br>";
-}
-echo "<br>";
+  }
+  echo "<br>";
 
-echo "<b>Create table " . $table . "</b><br>";
-mysql_query("CREATE TABLE IF NOT EXISTS " . $table . "(Name VARCHAR(20), Handler VARCHAR(20), Species VARCHAR(20), Gender CHAR(1), Bday DATE, Food VARCHAR(20));") or die(mysql_error());  
+  echo "<b>Create table " . $table . "</b><br>";
+  mysql_query("CREATE TABLE IF NOT EXISTS " . $table . "(Name VARCHAR(20), Handler VARCHAR(20), Species VARCHAR(20), Gender CHAR(1), Bday DATE, Food VARCHAR(20));") or die(mysql_error());  
 
-// Describe table.
-$query = "DESCRIBE " . $table;
-$res= mysql_query($query) or die('<b>Query failed:</b> ' . mysql_error() . '<br>');
-if($res) {
-   echo "<table border=1>\n";
-   for($i = 0; $i < mysql_num_fields($res); $i++) {
+  // Describe table.
+  $query = "DESCRIBE " . $table;
+  $res= mysql_query($query) or die('<b>Query failed:</b> ' . mysql_error() . '<br>');
+  if($res) {
+    echo "<table border=1>\n";
+    for($i = 0; $i < mysql_num_fields($res); $i++) {
       print '<th>' . mysql_field_name($res, $i) . '</th>';
-   }
-   while ($line = mysql_fetch_array($res, MYSQL_ASSOC)) {
-       echo "\t<tr>\n";
-       foreach ($line as $col_value) {
-           echo "\t\t<td>$col_value</td>\n";
-       }
-       echo "\t</tr>\n";
-   }
-   echo "</table>\n";
-}
+    }
+    while ($line = mysql_fetch_array($res, MYSQL_ASSOC)) {
+      echo "\t<tr>\n";
+      foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+      }
+      echo "\t</tr>\n";
+    }
+    echo "</table>\n";
+  }
 
-echo "<br>";
-// Populate table with sample data values.
-echo "<b>Insert values.</b><br>";
-mysql_query("INSERT INTO " . $table . " VALUES ('Oscar','Connie','cat','f','1999-03-30','sparrow');") or die(mysql_error());  
-mysql_query("INSERT INTO " . $table . " VALUES ('Missy','SueEllen','giraffe','f','1993-02-04','capncrunch');") or die(mysql_error());  
-mysql_query("INSERT INTO " . $table . " VALUES ('Orson','Deidre','hippopotamus','m','1994-03-17','birds');") or die(mysql_error());  
-mysql_query("INSERT INTO " . $table . " VALUES ('Bear','Jenn','dog','m','2002-06-26','berries');") or die(mysql_error());  
-mysql_query("INSERT INTO " . $table . " VALUES ('Stella','Calvin','monkey','f','2006-06-03','bananas');") or die(mysql_error());  
-mysql_query("INSERT INTO " . $table . " VALUES ('Calvin','RCompos','dog','m','2004-01-10','greenchili');") or die(mysql_error());  
+  echo "<br>";
+  // Populate table with sample data values.
+  echo "<b>Insert values.</b><br>";
+  mysql_query("INSERT INTO " . $table . " VALUES ('Oscar','Connie','cat','f','1999-03-30','sparrow');") or die(mysql_error());  
+  mysql_query("INSERT INTO " . $table . " VALUES ('Missy','SueEllen','giraffe','f','1993-02-04','capncrunch');") or die(mysql_error());  
+  mysql_query("INSERT INTO " . $table . " VALUES ('Orson','Deidre','hippopotamus','m','1994-03-17','birds');") or die(mysql_error());  
+  mysql_query("INSERT INTO " . $table . " VALUES ('Bear','Jenn','dog','m','2002-06-26','berries');") or die(mysql_error());  
+  mysql_query("INSERT INTO " . $table . " VALUES ('Stella','Calvin','monkey','f','2006-06-03','bananas');") or die(mysql_error());  
+  mysql_query("INSERT INTO " . $table . " VALUES ('Calvin','RCompos','dog','m','2004-01-10','greenchili');") or die(mysql_error());  
 
-// Print formatted output from query.
-echo "<b>select * from pet</b><br>";
-$res = mysql_query("select * from " . $table . ";") or die(mysql_error());  
-if($res) {
-   echo "<table border=1>\n";
-   for($i = 0; $i < mysql_num_fields($res); $i++) {
+  // Print formatted output from query.
+  echo "<b>select * from pet</b><br>";
+  $res = mysql_query("select * from " . $table . ";") or die(mysql_error());  
+  if($res) {
+    echo "<table border=1>\n";
+    for($i = 0; $i < mysql_num_fields($res); $i++) {
       print '<th>' . mysql_field_name($res, $i) . '</th>';
-   }
-   while ($line = mysql_fetch_array($res, MYSQL_ASSOC)) {
-       echo "\t<tr>\n";
-       foreach ($line as $col_value) {
-           echo "\t\t<td>$col_value</td>\n";
-       }
-       echo "\t</tr>\n";
-   }
-   echo "</table>\n";
-}
+    }
+    while ($line = mysql_fetch_array($res, MYSQL_ASSOC)) {
+      echo "\t<tr>\n";
+      foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+      }
+      echo "\t</tr>\n";
+    }
+    echo "</table>\n";
+  }
 
-echo "<b>Drop table " . $table . "</b><br>";
-mysql_query("DROP TABLE " . $table . ";") or die(mysql_error());  
+  echo "<b>Drop table " . $table . "</b><br>";
+  mysql_query("DROP TABLE " . $table . ";") or die(mysql_error());  
 
-mysql_close($link);
+  mysql_close($link);
 
-echo "<h3>Successful PHP and MySQL validation!</h3><br>";
+  echo "<h3>Successful PHP and MySQL validation!</h3><br>";
 
-}else{
-  echo "<form action=\"", $_SERVER['PHP_SELF'], "?p=test\" method=\"post\">";
-  echo "<p>";
-  echo "root MySQL password: <input type=\"password\" name=\"password\"><br></p>";
-  echo "<input type=\"submit\">";
-  echo "</form>";
+  }else{
+    echo "<form action=\"", $_SERVER['PHP_SELF'], "?p=test\" method=\"post\">";
+    echo "<p>";
+    echo "root MySQL password: <input type=\"password\" name=\"password\"><br></p>";
+    echo "<input type=\"submit\">";
+    echo "</form>";
+  }
 
-}
+  phpinfo();
 
 ?>
 
