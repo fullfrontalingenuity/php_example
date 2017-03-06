da5ab1dde152f71849076602297d64ab18a1e646
Paul Talbot <prolixalias@gmail.com>
2017-03-06 15:17:35 -0500

    testing DB username for devtest

--- include/membersite_config.php
+++ include/membersite_config.php
@@ -14,9 +14,9 @@ $fgmembersite->SetAdminEmail('admin@example.com');
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
 $fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'u_app1_prod',
+                      /*username*/'u_app1_devtest',
                       /*password*/'secret_password',
-                      /*database name*/'prod',
+                      /*database name*/'devtest',
                       /*table name*/'fgusers3');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
